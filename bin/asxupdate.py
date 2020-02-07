3fdaaff14224e4bd025e01bddd7ca6d8202140b5
divious1 <josehelps@gmail.com>
2020-02-06 20:08:20 -0500

    fixing a splunk entity error

--- bin/asxupdate.py
+++ bin/asxupdate.py
@@ -1,5 +1,6 @@
 import sys
 import splunk
+import splunk.entity as entity
 import splunklib.client
 import splunklib.results
 from splunklib.searchcommands import dispatch, GeneratingCommand, Configuration, Option, Boolean
@@ -27,7 +28,7 @@ class ASXUpdate(GeneratingCommand):
         results, dummyresults, settings = si.getOrganizedResults()
 
         # list api_key
-        ent = splunk.entity.getEntities('asx/conf/settings',namespace='Splunk_ASX', owner='nobody',sessionKey=settings['sessionKey'])
+        ent = entity.getEntities('asx/conf/settings',namespace='Splunk_ASX', owner='nobody',sessionKey=settings['sessionKey'])
 
         # return first set of cred
         for value in ent.values():
